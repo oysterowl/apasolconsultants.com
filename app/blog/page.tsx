c25b3ff6ff1a32f6b8505d00470f2fcd3f56ddc8
oysterowl <mohd.farhan@osiltec.com>
2025-09-08 15:17:28 +0800

    fix: remove unused Image import from blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,6 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Link from 'next/link';
-import Image from 'next/image';
 import { useState } from 'react';
 
 interface BlogPost {
